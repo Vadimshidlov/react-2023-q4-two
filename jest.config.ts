db6f49d392d8e54b2987b4b5667aa610269370c0
Vadimshidlov <vadim.shidlov@gmail.com>
2023-10-15 11:03:05 +0300

    chore: install jest enviroment

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+    '.+\\.(css|styl|less|sass|scss)$': 'jest-css-modules-transform',
+    // process `*.tsx` files with `ts-jest`
+  },
+  moduleNameMapper: {
+    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__ mocks __/fileMock.js',
+  },
+};
