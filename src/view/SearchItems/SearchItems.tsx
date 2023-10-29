13a3e363c236ccfe6b5246c3688cc709d9c4d3ba
Vadimshidlov <vadim.shidlov@gmail.com>
2023-10-29 14:25:29 +0300

    feat: migrate searchItems component to class component

--- src/view/SearchItems/SearchItems.tsx
+++ src/view/SearchItems/SearchItems.tsx
@@ -1,26 +1,32 @@
 import Hero from 'view/Hero/Hero';
 import MyLoader from 'view/MyLoader/MyLoader';
 import { PeopleRequestType } from 'view/Search/Search';
+import React from 'react';
 
 export type SearchItemsPropsType = {
   searchData: PeopleRequestType[] | undefined;
   isLoading: boolean;
 };
 
-function SearchItems({ searchData, isLoading }: SearchItemsPropsType) {
-  return (
-    <div className={isLoading ? 'searchItems__container__center' : 'searchItems__container'}>
-      <div>
+// eslint-disable-next-line react/prefer-stateless-function
+class SearchItems extends React.Component<SearchItemsPropsType> {
+  render() {
+    const { isLoading, searchData } = this.props;
+
+    return (
+      <div className={isLoading ? 'searchItems__container__center' : 'searchItems__container'}>
         <div>
-          {isLoading ? (
-            <MyLoader />
-          ) : (
-            searchData?.map((searchItem) => <Hero key={searchItem.name} heroData={searchItem} />)
-          )}
+          <div>
+            {isLoading ? (
+              <MyLoader />
+            ) : (
+              searchData?.map((searchItem) => <Hero key={searchItem.name} heroData={searchItem} />)
+            )}
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default SearchItems;
